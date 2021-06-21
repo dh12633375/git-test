f4cf1c4cb361e416051d47919dbea36f14a1e8d6
dinghang <461969777@qq.com>
2021-06-21 15:43:50 +0800

    second commit

--- src/main/java/com/dh/git/GitTest.java
+++ src/main/java/com/dh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.dh.git;
 public class GitTest {
     public static void main(String[] args) throws Exception {
         System.out.println("hello Git!");
+        System.out.println("hello GitHub!");
     }
 }
